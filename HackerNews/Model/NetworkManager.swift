d0519191f971ad7049ee2e46b48f1bc56ecb5a94
Jared Borders <jlbxsxs@gmail.com>
2020-05-09 23:37:56 -0500

    Create data model and network manager for hacker news posts from the API @ https://hn.algolia.com/api. Create UI with a navigation bar and list

---
+++ HackerNews/Model/NetworkManager.swift
@@ -0,0 +1,38 @@
+//
+//  NetworkManager.swift
+//  HackerNews
+//
+//  Created by Jared Borders on 5/9/20.
+//  Copyright © 2020 Jared Borders. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager: ObservableObject {
+    
+    @Published var posts = [Post]()
+    
+    func fetchData() {
+        if let url = URL(string: "https://hn.algolia.com/api/v1/search?tags=front_page") {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                } else {
+                    let decoder = JSONDecoder()
+                    if let safeData = data {
+                        do {
+                            let results = try decoder.decode(Results.self, from: safeData)
+                            DispatchQueue.main.async {
+                                self.posts = results.hits
+                            }
+                        } catch {
+                            print(error)
+                        }
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+}
